d59d7f37d28e32fd346eadba4fd30975fe19e612
Tomer Harari <tharari21@gmail.com>
2022-08-09 09:52:16 -0400

    commit tree f0a92ea4e222f6b803a513577853be42e340e21e

--- looping.rb
+++ looping.rb
@@ -1,5 +1,9 @@
 def happy_new_year
   # your code here
+	(10..1).step(-1).each do |i|
+		puts i	
+	end
+	puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -17,8 +21,16 @@ end
 
 def fizzbuzz_printer
   # your code here
+	(1..100).each { | i | puts fizzbuzz(i) }
 end
 
 def reverse_string(str)
   # your code here
+	new_str = ""
+	(0..str.length-1).each do |i|
+		new_str[i] = str[str.length-1-i]
+	end
+	return new_str
+
 end
+puts reverse_string("Tomer")
---
+++ test.rb
@@ -0,0 +1,9 @@
+10.times do |i|
+	puts "Loop " + i.to_s
+end
+
+10.times { |i| puts "Loop #{i} of 9" }
+(1..20).each do |i|
+	puts i
+end
+
